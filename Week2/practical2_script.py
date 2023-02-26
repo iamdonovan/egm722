51ee9816251e4c7ef331d6360fa7febd85faa642
Bob McNabb <robertmcnabb@gmail.com>
2023-02-26 14:03:12 +0000

    fix scalebar, general improvements to code, map, comments

--- Week2/practical2_script.py
+++ Week2/practical2_script.py
@@ -1,3 +1,4 @@
+import os
 import geopandas as gpd
 import matplotlib.pyplot as plt
 from cartopy.feature import ShapelyFeature
@@ -19,47 +20,46 @@ def generate_handles(labels, colors, edge='k', alpha=1):
 # adapted this question: https://stackoverflow.com/q/32333870
 # answered by SO user Siyh: https://stackoverflow.com/a/35705477
 def scale_bar(ax, location=(0.92, 0.95)):
-    llx0, llx1, lly0, lly1 = ax.get_extent(ccrs.PlateCarree())
-    sbllx = (llx1 + llx0) / 2
-    sblly = lly0 + (lly1 - lly0) * location[1]
-
-    tmc = ccrs.TransverseMercator(sbllx, sblly)
-    x0, x1, y0, y1 = ax.get_extent(tmc)
+    x0, x1, y0, y1 = ax.get_extent()
     sbx = x0 + (x1 - x0) * location[0]
     sby = y0 + (y1 - y0) * location[1]
 
-    ax.plot([sbx, sbx - 20000], [sby, sby], color='k', linewidth=9, transform=tmc)
-    ax.plot([sbx, sbx - 10000], [sby, sby], color='k', linewidth=6, transform=tmc)
-    ax.plot([sbx-10000, sbx - 20000], [sby, sby], color='w', linewidth=6, transform=tmc)
+    ax.plot([sbx, sbx - 20000], [sby, sby], color='k', linewidth=9, transform=ax.projection)
+    ax.plot([sbx, sbx - 10000], [sby, sby], color='k', linewidth=6, transform=ax.projection)
+    ax.plot([sbx-10000, sbx - 20000], [sby, sby], color='w', linewidth=6, transform=ax.projection)
 
-    ax.text(sbx, sby-4500, '20 km', transform=tmc, fontsize=8)
-    ax.text(sbx-12500, sby-4500, '10 km', transform=tmc, fontsize=8)
-    ax.text(sbx-24500, sby-4500, '0 km', transform=tmc, fontsize=8)
+    ax.text(sbx, sby-4500, '20 km', transform=ax.projection, fontsize=8)
+    ax.text(sbx-12500, sby-4500, '10 km', transform=ax.projection, fontsize=8)
+    ax.text(sbx-24500, sby-4500, '0 km', transform=ax.projection, fontsize=8)
 
+
+# load the outline of Northern Ireland for a backdrop
+outline = gpd.read_file(os.path.abspath('data_files/NI_outline.shp'))
 
 # load the datasets
-outline = gpd.read_file('data_files/NI_outline.shp')
-towns = gpd.read_file('data_files/Towns.shp')
-water = gpd.read_file('data_files/Water.shp')
-rivers = gpd.read_file('data_files/Rivers.shp')
-counties = gpd.read_file('data_files/Counties.shp')
+towns = gpd.read_file(os.path.abspath('data_files/Towns.shp'))
+water = gpd.read_file(os.path.abspath('data_files/Water.shp'))
+rivers = gpd.read_file(os.path.abspath('data_files/Rivers.shp'))
+counties = gpd.read_file(os.path.abspath('data_files/Counties.shp'))
 
 # create a figure of size 10x10 (representing the page size in inches)
 myFig = plt.figure(figsize=(10, 10))
 
 myCRS = ccrs.UTM(29)  # create a Universal Transverse Mercator reference system to transform our data.
+# NI is in UTM Zone 29, so we pass 29 to ccrs.UTM()
 
-ax = plt.axes(projection=ccrs.Mercator())  # finally, create an axes object in the figure, using a Mercator
-# projection, where we can actually plot our data.
+ax = plt.axes(projection=myCRS)  # finally, create an axes object in the figure, using a UTM projection,
+# where we can actually plot our data.
 
 # first, we just add the outline of Northern Ireland using cartopy's ShapelyFeature
 outline_feature = ShapelyFeature(outline['geometry'], myCRS, edgecolor='k', facecolor='w')
 
 xmin, ymin, xmax, ymax = outline.total_bounds
-ax.add_feature(outline_feature) # add the features we've created to the map.
+ax.add_feature(outline_feature)  # add the features we've created to the map.
 
 # using the boundary of the shapefile features, zoom the map to our area of interest
-ax.set_extent([xmin, xmax, ymin, ymax], crs=myCRS) # because total_bounds gives output as xmin, ymin, xmax, ymax,
+ax.set_extent([xmin-5000, xmax+5000, ymin-5000, ymax+5000], crs=myCRS)  # because total_bounds
+# gives output as xmin, ymin, xmax, ymax,
 # but set_extent takes xmin, xmax, ymin, ymax, we re-order the coordinates here.
 
 # pick colors, add features to the map
@@ -70,30 +70,32 @@ county_names = list(counties.CountyName.unique())
 county_names.sort()  # sort the counties alphabetically by name
 
 # next, add the municipal outlines to the map using the colors that we've picked.
-# here, we're iterating over the list of names we created above
+# here, we're iterating over the unique values in the 'CountyName' field.
 # we're also setting the edge color to be black, with a line width of 0.5 pt.
 # Feel free to experiment with different colors and line widths.
-for i, name in enumerate(county_names):
-    feat = ShapelyFeature(counties['geometry'][counties['CountyName'] == name], myCRS,
-                          edgecolor='k',
-                          facecolor=county_colors[i],
-                          linewidth=1,
-                          alpha=0.25)
-    ax.add_feature(feat)
+for ii, name in enumerate(county_names):
+    feat = ShapelyFeature(counties.loc[counties['CountyName'] == name, 'geometry'],  # first argument is the geometry
+                          myCRS,  # second argument is the CRS
+                          edgecolor='k',  # outline the feature in black
+                          facecolor=county_colors[ii],  # set the face color to the corresponding color from the list
+                          linewidth=1,  # set the outline width to be 1 pt
+                          alpha=0.25)  # set the alpha (transparency) to be 0.25 (out of 1)
+    ax.add_feature(feat)  # once we have created the feature, we have to add it to the map using ax.add_feature()
 
 # here, we're setting the edge color to be the same as the face color. Feel free to change this around,
 # and experiment with different line widths.
-water_feat = ShapelyFeature(water['geometry'], myCRS,
-                            edgecolor='mediumblue',
-                            facecolor='mediumblue',
-                            linewidth=1)
-ax.add_feature(water_feat)
+water_feat = ShapelyFeature(water['geometry'],  # first argument is the geometry
+                            myCRS,  # second argument is the CRS
+                            edgecolor='mediumblue',  # set the edgecolor to be mediumblue
+                            facecolor='mediumblue',  # set the facecolor to be mediumblue
+                            linewidth=1)  # set the outline width to be 1 pt
+ax.add_feature(water_feat)  # add the collection of features to the map
 
-river_feat = ShapelyFeature(rivers['geometry'], myCRS,
-                            edgecolor='royalblue',
-                            linewidth=0.2)
-
-ax.add_feature(river_feat)
+river_feat = ShapelyFeature(rivers['geometry'],  # first argument is the geometry
+                            myCRS,  # second argument is the CRS
+                            edgecolor='royalblue',  # set the edgecolor to be royalblue
+                            linewidth=0.2)  # set the linewidth to be 0.2 pt
+ax.add_feature(river_feat)  # add the collection of features to the map
 
 # ShapelyFeature creates a polygon, so for point data we can just use ax.plot()
 town_handle = ax.plot(towns.geometry.x, towns.geometry.y, 's', color='0.5', ms=6, transform=myCRS)
@@ -114,22 +116,22 @@ nice_names = [name.title() for name in county_names]
 handles = county_handles + water_handle + river_handle + town_handle
 labels = nice_names + ['Lakes', 'Rivers', 'Towns']
 
-leg = ax.legend(handles, labels, title='Legend', title_fontsize=14,
-                 fontsize=12, loc='upper left', frameon=True, framealpha=1)
+leg = ax.legend(handles, labels, title='Legend', title_fontsize=12,
+                fontsize=10, loc='upper left', frameon=True, framealpha=1)
 
-gridlines = ax.gridlines(draw_labels=True,
-                         xlocs=[-8, -7.5, -7, -6.5, -6, -5.5],
-                         ylocs=[54, 54.5, 55, 55.5])
-
-gridlines.left_labels = False
-gridlines.bottom_labels = False
-ax.set_extent([xmin, xmax, ymin, ymax], crs=myCRS)
+gridlines = ax.gridlines(draw_labels=True,  # draw  labels for the grid lines
+                         xlocs=[-8, -7.5, -7, -6.5, -6, -5.5],  # add longitude lines at 0.5 deg intervals
+                         ylocs=[54, 54.5, 55, 55.5])  # add latitude lines at 0.5 deg intervals
+gridlines.left_labels = False  # turn off the left-side labels
+gridlines.bottom_labels = False  # turn off the bottom labels
 
 # add the text labels for the towns
-for i, row in towns.iterrows():
-    x, y = row.geometry.x, row.geometry.y
-    ax.text(x, y, row['TOWN_NAME'].title(), fontsize=8, transform=myCRS) # use plt.text to place a label at x,y
+for ind, row in towns.iterrows():  # towns.iterrows() returns the index and row
+    x, y = row.geometry.x, row.geometry.y  # get the x,y location for each town
+    ax.text(x, y, row['TOWN_NAME'].title(), fontsize=8, transform=myCRS)  # use plt.text to place a label at x,y
 
+# add the scale bar to the axis
 scale_bar(ax)
 
+# save the figure as map.png, cropped to the axis (bbox_inches='tight'), and a dpi of 300
 myFig.savefig('map.png', bbox_inches='tight', dpi=300)
