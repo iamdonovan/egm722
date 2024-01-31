70dbf11adfcae6479feb19b5e1f555d9f0330c69
Bob McNabb <robertmcnabb@gmail.com>
2024-01-31 11:48:38 +0000

    add updated plotting code from week3_example.py

--- Week3/exercise_script.py
+++ Week3/exercise_script.py
@@ -18,6 +18,7 @@ import matplotlib.patches as mpatches
 # below here, you may need to modify the script somewhat to create your map.
 # create a crs using ccrs.UTM() that corresponds to our CRS
 ni_utm = ccrs.UTM(29)
+
 # create a figure of size 10x10 (representing the page size in inches
 fig, ax = plt.subplots(1, 1, figsize=(10, 10), subplot_kw=dict(projection=ni_utm))
 
@@ -32,15 +33,17 @@ gridlines.bottom_labels = False
 divider = make_axes_locatable(ax)
 cax = divider.append_axes("right", size="5%", pad=0.1, axes_class=plt.Axes)
 
-# plot the ward data into our axis, using
+# plot the ward data into our axis, using gdf.plot()
 ward_plot = wards.plot(column='Population', ax=ax, vmin=1000, vmax=8000, cmap='viridis',
                        legend=True, cax=cax, legend_kwds={'label': 'Resident Population'})
 
-county_outlines = ShapelyFeature(counties['geometry'], myCRS, edgecolor='r', facecolor='none')
-
+# add county outlines in red using ShapelyFeature
+county_outlines = ShapelyFeature(counties['geometry'], ni_utm, edgecolor='r', facecolor='none')
 ax.add_feature(county_outlines)
-county_handles = [mpatches.Rectangle((0, 0), 1, 1, facecolor='none', edgecolor='r')]
 
+county_handles = generate_handles([''], ['none'], edge='r')
+
+# add a legend in the upper left-hand corner
 ax.legend(county_handles, ['County Boundaries'], fontsize=12, loc='upper left', framealpha=1)
 
 # save the figure
