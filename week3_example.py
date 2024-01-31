87a1406a5ff782f31c8080cfb743e89012657285
Bob McNabb <robertmcnabb@gmail.com>
2024-01-31 11:47:25 +0000

    fix the script so that it actually works

--- week3_example.py
+++ week3_example.py
@@ -23,20 +23,22 @@ plt.ion()
 # try to print the results to the screen using the format method demonstrated in the workbook
 
 # load the necessary data here and transform to a UTM projection
-wards = gpd.read_file('../workbooks/Week3/data_files/NI_Wards.shp').to_crs(epsg=32629)
-counties = gpd.read_file('../workbooks/Week3/data_files/Counties.shp').to_crs(epsg=32629)
+wards = gpd.read_file('Week3/data_files/NI_Wards.shp').to_crs(epsg=32629)
+counties = gpd.read_file('Week3/data_files/Counties.shp').to_crs(epsg=32629)
 # your analysis goes here...
 
+# do a spatial join of wards and counties
 join = gpd.sjoin(wards, counties, how='inner', lsuffix='left', rsuffix='right')
 
-summary = clipped_gdf.groupby(['CountyName'])['Population'].sum()
-
+# print a summary of the population for each county, based on the electoral wards
+summary = join.groupby(['CountyName'])['Population'].sum()
 print(summary)
 
 # ---------------------------------------------------------------------------------------------------------------------
 # below here, you may need to modify the script somewhat to create your map.
 # create a crs using ccrs.UTM() that corresponds to our CRS
 ni_utm = ccrs.UTM(29)
+
 # create a figure of size 10x10 (representing the page size in inches
 fig, ax = plt.subplots(1, 1, figsize=(10, 10), subplot_kw=dict(projection=ni_utm))
 
@@ -51,17 +53,19 @@ gridlines.bottom_labels = False
 divider = make_axes_locatable(ax)
 cax = divider.append_axes("right", size="5%", pad=0.1, axes_class=plt.Axes)
 
-# plot the ward data into our axis, using
+# plot the ward data into our axis, using gdf.plot()
 ward_plot = wards.plot(column='Population', ax=ax, vmin=1000, vmax=8000, cmap='viridis',
                        legend=True, cax=cax, legend_kwds={'label': 'Resident Population'})
 
-county_outlines = ShapelyFeature(counties['geometry'], myCRS, edgecolor='r', facecolor='none')
-
+# add county outlines in red using ShapelyFeature
+county_outlines = ShapelyFeature(counties['geometry'], ni_utm, edgecolor='r', facecolor='none')
 ax.add_feature(county_outlines)
+
 county_handles = generate_handles([''], ['none'], edge='r')
 
+# add a legend in the upper left-hand corner
 ax.legend(county_handles, ['County Boundaries'], fontsize=12, loc='upper left', framealpha=1)
 
 # save the figure
-# fig.savefig('sample_map.png', dpi=300, bbox_inches='tight')
+fig.savefig('sample_map.png', dpi=300, bbox_inches='tight')
 
